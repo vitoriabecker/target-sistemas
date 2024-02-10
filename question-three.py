8a616ab84932a0430bf01d7a1146bb7f00c0489b
vitoriabecker <109805274+vitoriabecker@users.noreply.github.com>
2024-02-10 00:05:05 -0300

    fixed question three

--- question-three.py
+++ question-three.py
@@ -7,4 +7,50 @@
 # a) Usar o json ou xml disponível como fonte dos dados do faturamento mensal;
 # b) Podem existir dias sem faturamento, como nos finais de semana e feriados. Estes dias devem ser ignorados no cálculo da média;
 
+import json
 
+file = open("dados.json")
+dados = json.load(file)
+
+def get_menor_valor(dados):
+  menor = dados[0].get('valor')
+
+  for i in range(len(dados)):
+    if dados[i].get('valor') < menor:
+      menor = dados[i].get('valor')
+    
+  return menor
+    
+def get_maior_valor(dados):
+  maior = dados[0].get('valor')
+
+  for i in range(len(dados)):
+    if dados[i].get('valor') > maior:
+      maior = dados[i].get('valor')
+    
+  return maior
+
+def get_superior_media(dados):
+  valores = []
+  for i in range(len(dados)):
+    if (dados[i].get('valor') != 0):
+      valores.append(dados[i].get('valor'))
+
+  valor_total = sum(valores)
+  media_mensal = valor_total/len(valores)
+
+  maior_que_media = 0
+  for i in range(len(valores)):
+    if valores[i] > media_mensal:
+      maior_que_media +=1
+    
+  return maior_que_media
+
+menor_valor = get_menor_valor(dados)
+print(f'Menor valor de faturamento no mês: R$ {menor_valor:.2f}')
+
+maior_valor = get_maior_valor(dados)
+print(f'Maior valor de faturamento no mês: R$ {maior_valor:.2f}')
+
+dia_superior_media = get_superior_media(dados)
+print(f'Quantidade de dias em que o faturamento foi superior ao da média mensal: {dia_superior_media}')
